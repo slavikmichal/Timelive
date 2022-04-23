60376463c5ee8d5839e40ff687b6904f067c5e94
Michal Slavík <xslavik@fi.muni.cz>
2022-04-23 22:15:25 +0200

    web/mobile fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:timelive/timelive_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyCIRFX3FoDpuwiC5UOomeOOLgNW2Bdcr6A",
+            authDomain: "timelive-dd430.firebaseapp.com",
+            projectId: "timelive-dd430",
+            storageBucket: "timelive-dd430.appspot.com",
+            messagingSenderId: "67776575253",
+            appId: "1:67776575253:web:502e48588e2ba707ee9b1c"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const TimeliveApp());
 }
