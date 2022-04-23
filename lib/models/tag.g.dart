8a42e69d94e51b65b359ef842b0270f1f81477da
Noro <bodnar@inqool.cz>
2022-04-23 20:16:31 +0200

    Added color to tag

--- lib/models/tag.g.dart
+++ lib/models/tag.g.dart
@@ -10,10 +10,12 @@ Tag _$TagFromJson(Map<String, dynamic> json) => Tag(
       json['id'] as String,
       json['name'] as String,
       json['counter'] as int,
+      json['color'] as int,
     );
 
 Map<String, dynamic> _$TagToJson(Tag instance) => <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
       'counter': instance.counter,
+      'color': instance.color,
     };
