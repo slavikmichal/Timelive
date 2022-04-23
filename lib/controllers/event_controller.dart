f794f616d45eb75b08e021d16d2b1681beae14f6
Martin Krsak <xkrsak@fi.muni.cz>
2022-04-23 19:34:00 +0200

    EventFormState model; New tag constructor counter =  0; Event controller added

---
+++ lib/controllers/event_controller.dart
@@ -0,0 +1,40 @@
+ import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:timelive/models/event.dart';
+import 'package:timelive/models/event_form_state.dart';
+ import 'package:timelive/models/tag.dart';
+
+class EventController {
+  static final CollectionReference _eventsRef = FirebaseFirestore.instance.collection('events');
+  static final CollectionReference _tagsRef = FirebaseFirestore.instance.collection('tags');
+  
+
+  Future<void> addEvent(EventFormState formState) {
+    var event = Event(formState.name, formState.description, formState.date, formState.tags);
+    
+    formState.tags.forEach((tag) {
+      var incr_tag = Tag(tag.id, tag.name, tag.counter+1);
+      if (tag.counter > 1) { //tag exists, increase counter
+        _tagsRef
+            .where('id', isEqualTo: tag.id)
+            .get()
+            .then((QuerySnapshot value) {
+              value.docs.forEach((doc) {
+                  _tagsRef.doc(doc.id).update(incr_tag.toJson())
+                    .then((value) => print('Tag updated'))
+                    .catchError((error) => print('Failed to update tag -- $error'));
+              });
+            });
+      } else { //insert new tag
+        _tagsRef.add(incr_tag.toJson())
+            .then((value) => print('Tag inserted'))
+            .catchError((error) => print('Failed to insert tag -- $error'));
+      }
+    });
+
+    // TODO save files
+
+    return _eventsRef.add(event.toJson())
+       .then((value) => print('Event inserted'))
+       .catchError((error) => print('Failed to insert event -- $error'));
+  }
+}
