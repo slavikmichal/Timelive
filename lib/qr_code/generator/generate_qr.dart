47b0b80b895237618e3b487c10c93247c8247ad4
Adam Radvan <radvan@inqool.cz>
2022-04-23 17:12:40 +0200

    qr generate +scan

---
+++ lib/qr_code/generator/generate_qr.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:timelive/qr_code/generator/qr_generator.dart';
+
+void main() => runApp(GenerateQrExample());
+
+/// The example application class
+class GenerateQrExample extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+    return MaterialApp(
+      title: 'QR.Flutter',
+      theme: ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      home: GenerateQrCode(),
+    );
+  }
+}
+
+/// This is the screen that you'll see when the app starts
+class GenerateQrCode extends StatefulWidget {
+  @override
+  _GenerateQrCodeState createState() => _GenerateQrCodeState();
+}
+
+class _GenerateQrCodeState extends State<GenerateQrCode> {
+  @override
+  Widget build(BuildContext context) {
+    const message = 'HelloWorld';
+    const size = 360.0;
+
+    return Material(
+      color: Colors.white,
+      child: SafeArea(
+        top: true,
+        bottom: true,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: QrCodeFactory.generateSimple(message, size),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40)
+                  .copyWith(bottom: 40),
+              child: const Text(message),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
