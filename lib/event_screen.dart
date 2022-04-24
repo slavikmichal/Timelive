9ada660df4e6b838bdd7ab8947b72eadc7c96ce1
Martin Krsak <xkrsak@fi.muni.cz>
2022-04-24 06:04:03 +0200

    Load files from firestorage

--- lib/event_screen.dart
+++ lib/event_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -24,17 +25,17 @@ class EventScreen extends StatefulWidget {
 class _EventScreenState extends State<EventScreen> {
   static const textShift = 0.19988425925925924769188314471879;
 
-  List<String> kDemoImages = [
-    'https://i.pinimg.com/originals/7f/91/a1/7f91a18bcfbc35570c82063da8575be8.jpg',
-    'https://www.absolutearts.com/portfolio3/a/afifaridasiddique/Still_Life-1545967888l.jpg',
-    'https://cdn11.bigcommerce.com/s-x49po/images/stencil/1280x1280/products/53415/72138/1597120261997_IMG_20200811_095922__49127.1597493165.jpg?c=2',
-    'https://i.pinimg.com/originals/47/7e/15/477e155db1f8f981c4abb6b2f0092836.jpg',
-    'https://images.saatchiart.com/saatchi/770124/art/3760260/2830144-QFPTZRUH-7.jpg',
-    'https://images.unsplash.com/photo-1471943311424-646960669fbc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c3RpbGwlMjBsaWZlfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80',
-    'https://cdn11.bigcommerce.com/s-x49po/images/stencil/1280x1280/products/40895/55777/1526876829723_P211_24X36__2018_Stilllife_15000_20090__91926.1563511650.jpg?c=2',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIUsxpakPiqVF4W_rOlq6eoLYboOFoxw45qw&usqp=CAU',
-    'https://images.mojarto.com/photos/267893/large/DA-SL-01.jpg?1560834975',
-  ];
+  // List<String> kDemoImages = [
+  //   'https://i.pinimg.com/originals/7f/91/a1/7f91a18bcfbc35570c82063da8575be8.jpg',
+  //   'https://www.absolutearts.com/portfolio3/a/afifaridasiddique/Still_Life-1545967888l.jpg',
+  //   'https://cdn11.bigcommerce.com/s-x49po/images/stencil/1280x1280/products/53415/72138/1597120261997_IMG_20200811_095922__49127.1597493165.jpg?c=2',
+  //   'https://i.pinimg.com/originals/47/7e/15/477e155db1f8f981c4abb6b2f0092836.jpg',
+  //   'https://images.saatchiart.com/saatchi/770124/art/3760260/2830144-QFPTZRUH-7.jpg',
+  //   'https://images.unsplash.com/photo-1471943311424-646960669fbc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c3RpbGwlMjBsaWZlfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80',
+  //   'https://cdn11.bigcommerce.com/s-x49po/images/stencil/1280x1280/products/40895/55777/1526876829723_P211_24X36__2018_Stilllife_15000_20090__91926.1563511650.jpg?c=2',
+  //   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIUsxpakPiqVF4W_rOlq6eoLYboOFoxw45qw&usqp=CAU',
+  //   'https://images.mojarto.com/photos/267893/large/DA-SL-01.jpg?1560834975',
+  // ];
 
   // Scroll controller for carousel
   late InfiniteScrollController _controller;
@@ -79,6 +80,7 @@ class _EventScreenState extends State<EventScreen> {
       // backgroundColor: Colors.black,
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 20),
             Hero(
@@ -106,54 +108,11 @@ class _EventScreenState extends State<EventScreen> {
                 ),
                 SizedBox(
                   height: 200,
-                  child: InfiniteCarousel.builder(
-                    itemCount: kDemoImages.length,
-                    itemExtent: _itemExtent ?? 40,
-                    scrollBehavior: kIsWeb
-                        ? ScrollConfiguration.of(context).copyWith(
-                            dragDevices: {
-                              // Allows to swipe in web browsers
-                              PointerDeviceKind.touch,
-                              PointerDeviceKind.mouse
-                            },
-                          )
-                        : null,
-                    loop: true,
-                    controller: _controller,
-                    onIndexChanged: (index) {
-                      if (_selectedIndex != index) {
-                        setState(() {
-                          _selectedIndex = index;
-                        });
-                      }
-                    },
-                    itemBuilder: (context, itemIndex, realIndex) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                        child: GestureDetector(
-                          onTap: () {
-                            _controller.animateToItem(realIndex);
-                            Navigator.push(context, MaterialPageRoute(builder: (context) {
-                              return ImagesDetailScreen(
-                                urls: kDemoImages,
-                                startIndex: itemIndex,
-                              );
-                            }));
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              boxShadow: kElevationToShadow[2],
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                  kDemoImages[itemIndex],
-                                ),
-                                fit: BoxFit.fill,
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
+                  child: FutureBuilder(
+                    future: _loadImages(),
+                    builder: (context, AsyncSnapshot<List<String>> snapshot) {
+                      var urls = snapshot.data ?? [];
+                      return urls.isNotEmpty ? _carousel(context, urls) : const Text('');
                     },
                   ),
                 ),
@@ -163,5 +122,75 @@ class _EventScreenState extends State<EventScreen> {
         ),
       ),
     );
+  }
+
+  Widget _carousel(BuildContext context, List<String> data) {
+    return InfiniteCarousel.builder(
+      itemCount: data.length,
+      itemExtent: _itemExtent ?? 40,
+      scrollBehavior: kIsWeb
+          ? ScrollConfiguration.of(context).copyWith(
+              dragDevices: {
+                // Allows to swipe in web browsers
+                PointerDeviceKind.touch,
+                PointerDeviceKind.mouse
+              },
+            )
+          : null,
+      loop: true,
+      controller: _controller,
+      onIndexChanged: (index) {
+        if (_selectedIndex != index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        }
+      },
+      itemBuilder: (context, itemIndex, realIndex) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          child: GestureDetector(
+            onTap: () {
+              _controller.animateToItem(realIndex);
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return ImagesDetailScreen(
+                  urls: data,
+                  startIndex: itemIndex,
+                );
+              }));
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                boxShadow: kElevationToShadow[2],
+                image: DecorationImage(
+                  image: NetworkImage(
+                    data[itemIndex],
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<List<String>> _loadImages() async {
+    String event_id = widget.event.id ?? '99999999999';
+    var _storageRef = FirebaseStorage.instance.ref().child(event_id);
+
+      List<String> paths = [];
+
+      final ListResult result = await _storageRef.list();
+      final List<Reference> allFiles = result.items;
+
+      await Future.forEach<Reference>(allFiles, (file) async {
+        final String fileUrl = await file.getDownloadURL();
+        paths.add(fileUrl);
+      });
+
+      return paths;
   }
 }
